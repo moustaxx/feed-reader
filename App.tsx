b288a4daaf42d38d327642d5ab7ef1612f676174
moustaxx <maciasm1303@gmail.com>
2019-06-27 12:07:38 +0200

    Prevent using react-native-screens in web

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { useScreens } from 'react-native-screens'; // eslint-disable-line import/no-unresolved
 
 import Main from './src/Main';
 import theme from './src/theme';
 
-useScreens();
+if (Platform.OS !== 'web') useScreens();
 
 const WrappedApp = () => {
 	return (

2b958464eee4dad7dda8a4f2a94a514c41cfea4c
moustaxx <maciasm1303@gmail.com>
2019-12-12 19:05:01 +0100

    Upgrade expo

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import { Platform } from 'react-native';
-import { useScreens } from 'react-native-screens'; // eslint-disable-line import/no-unresolved
+import { enableScreens } from 'react-native-screens'; // eslint-disable-line import/no-unresolved
 import Main from './src/Main';
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-if (Platform.OS !== 'web') useScreens();
+if (Platform.OS !== 'web') enableScreens();
 
 export default Main;
